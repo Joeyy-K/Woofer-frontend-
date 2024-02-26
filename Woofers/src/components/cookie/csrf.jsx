2de4460c3806076c38d72e7a75ac4fad9b629377
unknown <joeykkdr@gmail.com>
2024-02-26 05:29:51 -0800

    removed console.log from csrf.jsx

--- Woofers/src/components/cookie/csrf.jsx
+++ Woofers/src/components/cookie/csrf.jsx
@@ -3,7 +3,6 @@ import { API_URL } from "../url/url";
 export function fetchCSRFToken() {
     fetch(`${API_URL}/csrf/`)
       .then(response => response.json())
-      .then(data => console.log(data))
       .catch((error) => {
         console.error('Error:', error);
       });
